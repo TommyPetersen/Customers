84b5d11b1e37d8140c5708df844e2cfe3c9117ef
Tommy Petersen <tp@aia.dk>
2025-06-08 11:07:22 +0200

    Omdoebninger til dansk.

--- Clojure/customers/src/customers/anmoder_til_spilsystem.clj
+++ Clojure/customers/src/customers/anmoder_til_spilsystem.clj
@@ -9,47 +9,47 @@
   (println "\t*** Spiller1 ***")
   (println "\tValg\tBeskrivelse")
   (println "\t----\t-----------")
-  (println "\t1\t[Traek en vinder] Ikke-interaktiv spiller som \"local-clojure-agent\"")
+  (println "\t1\t[Traek en vinder] Ikke-interaktiv spiller som \"lokal-clojureagent\"")
   (println "\t2\t[Traek en vinder] Ikke-interaktiv spiller som \"http webservice paa localhost port 3201\"")
   (println "\t3\t[Traek en vinder] Interaktiv tekstbaseret spiller (forudvalgt)")
   (println "\t\t")
   (println "\t4\t[Fire paa stribe] Interaktiv tekstbaseret spiller")
   (println "\t5\t[Fire paa stribe] Interaktiv grafikbaseret spiller")
-  (println "\t6\t[Fire paa stribe] Ikke-interaktiv spiller baseret paa tilfaeldige traek som \"local-clojure-agent\"")
+  (println "\t6\t[Fire paa stribe] Ikke-interaktiv spiller baseret paa tilfaeldige traek som \"lokal-clojureagent\"")
   (println "\t7\t[Fire paa stribe] Ikke-interaktiv spiller baseret paa tilfaeldige traek som \"http webservice paa localhost port 3003\"")
-  (println "\t8\t[Fire paa stribe] Ikke-interaktiv spiller baseret paa minimax som \"local-clojure-agent\"")
+  (println "\t8\t[Fire paa stribe] Ikke-interaktiv spiller baseret paa minimax som \"lokal-clojureagent\"")
   (println "\t9\t[Fire paa stribe] Ikke-interaktiv spiller baseret paa minimax som \"http webservice paa localhost port 3001\"")
   (println "\t\t")
   (println "\t10\t[Infektion] Interaktiv tekstbaseret spiller")
   (println "\t11\t[Infektion] Interaktiv grafikbaseret spiller")
-  (println "\t12\t[Infektion] Ikke-interaktiv spiller baseret paa tilfaeldige traek som \"local-clojure-agent\"")
+  (println "\t12\t[Infektion] Ikke-interaktiv spiller baseret paa tilfaeldige traek som \"lokal-clojureagent\"")
   (println "\t13\t[Infektion] Ikke-interaktiv spiller baseret paa tilfaeldige traek som \"http webservice paa localhost port 3103\"")
-  (println "\t14\t[Infektion] Ikke-interaktiv spiller baseret paa minimax som \"local-clojure-agent\"")
+  (println "\t14\t[Infektion] Ikke-interaktiv spiller baseret paa minimax som \"lokal-clojureagent\"")
   (println "\t15\t[Infektion] Ikke-interaktiv spiller baseret paa minimax som \"http webservice paa localhost port 3101\"")
   (println "\tAndet\t*forudvalgt*")
   (print "\tVaelg spiller: ")
   (flush)
   (let [player-choice (read-line)]
     (case player-choice
-      "1" "local-clojure-agent://games.draw-a-winner.ikke-interaktiv-spiller1/spiller1"
+      "1" "lokal-clojureagent://games.draw-a-winner.ikke-interaktiv-spiller1/spiller1"
       "2" "http://localhost:3201"
-      "3" "local-clojure-agent://games.draw-a-winner.interaktiv-tekstbaseret-spiller/spiller1"
+      "3" "lokal-clojureagent://games.draw-a-winner.interaktiv-tekstbaseret-spiller/spiller1"
       
-      "4" "local-clojure-agent://games.connect-four.interaktiv-tekstbaseret-spiller/spiller1"
-      "5" "local-clojure-agent://games.connect-four.interaktiv-grafikbaseret-spiller/spiller1"
-      "6" "local-clojure-agent://games.connect-four.ikke-interaktiv-spiller-baseret-paa-tilfaeldige-traek/spiller1"
+      "4" "lokal-clojureagent://games.connect-four.interaktiv-tekstbaseret-spiller/spiller1"
+      "5" "lokal-clojureagent://games.connect-four.interaktiv-grafikbaseret-spiller/spiller1"
+      "6" "lokal-clojureagent://games.connect-four.ikke-interaktiv-spiller-baseret-paa-tilfaeldige-traek/spiller1"
       "7" "http://localhost:3003"
-      "8" "local-clojure-agent://games.connect-four.ikke-interaktiv-spiller-baseret-paa-minimax/spiller1"
+      "8" "lokal-clojureagent://games.connect-four.ikke-interaktiv-spiller-baseret-paa-minimax/spiller1"
       "9" "http://localhost:3001"
       
-      "10" "local-clojure-agent://games.infection.interaktiv-tekstbaseret-spiller/spiller1"
-      "11" "local-clojure-agent://games.infection.interaktiv-grafikbaseret-spiller/spiller1"
-      "12" "local-clojure-agent://games.infection.ikke-interaktiv-spiller-baseret-paa-tilfaeldige-traek/spiller1"
+      "10" "lokal-clojureagent://games.infection.interaktiv-tekstbaseret-spiller/spiller1"
+      "11" "lokal-clojureagent://games.infection.interaktiv-grafikbaseret-spiller/spiller1"
+      "12" "lokal-clojureagent://games.infection.ikke-interaktiv-spiller-baseret-paa-tilfaeldige-traek/spiller1"
       "13" "http://localhost:3103"
-      "14" "local-clojure-agent://games.infection.ikke-interaktiv-spiller-baseret-paa-minimax/spiller1"
+      "14" "lokal-clojureagent://games.infection.ikke-interaktiv-spiller-baseret-paa-minimax/spiller1"
       "15" "http://localhost:3101"
       
-      "local-clojure-agent://games.draw-a-winner.interaktiv-tekstbaseret-spiller/spiller1"
+      "lokal-clojureagent://games.draw-a-winner.interaktiv-tekstbaseret-spiller/spiller1"
     )
   )
 )
@@ -59,47 +59,47 @@
   (println "\t*** Spiller2 ***")
   (println "\tValg\tBeskrivelse")
   (println "\t----\t-----------")
-  (println "\t1\t[Traek en vinder] Ikke-interaktiv spiller som \"local-clojure-agent\" (forudvalgt)")
+  (println "\t1\t[Traek en vinder] Ikke-interaktiv spiller som \"lokal-clojureagent\" (forudvalgt)")
   (println "\t2\t[Traek en vinder] Ikke-interaktiv spiller som \"http webservice paa localhost port 3202\"")
   (println "\t3\t[Traek en vinder] Interaktiv tekstbaseret spiller")
   (println "\t\t")
   (println "\t4\t[Fire paa stribe] Interaktiv tekstbaseret spiller")
   (println "\t5\t[Fire paa stribe] Interaktiv grafikbaseret spiller")
-  (println "\t6\t[Fire paa stribe] Ikke-interaktiv spiller baseret paa tilfaeldige traek som \"local-clojure-agent\"")
+  (println "\t6\t[Fire paa stribe] Ikke-interaktiv spiller baseret paa tilfaeldige traek som \"lokal-clojureagent\"")
   (println "\t7\t[Fire paa stribe] Ikke-interaktiv spiller baseret paa tilfaeldige traek som \"http webservice paa localhost port 3004\"")
-  (println "\t8\t[Fire paa stribe] Ikke-interaktiv spiller baseret paa minimax som \"local-clojure-agent\"")
+  (println "\t8\t[Fire paa stribe] Ikke-interaktiv spiller baseret paa minimax som \"lokal-clojureagent\"")
   (println "\t9\t[Fire paa stribe] Ikke-interaktiv spiller baseret paa minimax som \"http webservice paa localhost port 3002\"")
   (println "\t\t")
   (println "\t10\t[Infektion] Interaktiv tekstbaseret spiller")
   (println "\t11\t[Infektion] Interaktiv grafikbaseret spiller")
-  (println "\t12\t[Infektion] Ikke-interaktiv spiller baseret paa tilfaeldige traek som \"local-clojure-agent\"")
+  (println "\t12\t[Infektion] Ikke-interaktiv spiller baseret paa tilfaeldige traek som \"lokal-clojureagent\"")
   (println "\t13\t[Infektion] Ikke-interaktiv spiller baseret paa tilfaeldige traek som \"http webservice paa localhost port 3104\"")
-  (println "\t14\t[Infektion] Ikke-interaktiv spiller baseret paa minimax som \"local-clojure-agent\"")
+  (println "\t14\t[Infektion] Ikke-interaktiv spiller baseret paa minimax som \"lokal-clojureagent\"")
   (println "\t15\t[Infektion] Ikke-interaktiv spiller baseret paa minimax som \"http webservice paa localhost port 3102\"")
   (println "\tAndet\t*forudvalgt*")
   (print "\tVaelg spiller: ")
   (flush)
   (let [player-choice (read-line)]
     (case player-choice
-      "1" "local-clojure-agent://games.draw-a-winner.ikke-interaktiv-spiller2/spiller2"
+      "1" "lokal-clojureagent://games.draw-a-winner.ikke-interaktiv-spiller2/spiller2"
       "2" "http://localhost:3202"
-      "3" "local-clojure-agent://games.draw-a-winner.interaktiv-tekstbaseret-spiller/spiller2"
+      "3" "lokal-clojureagent://games.draw-a-winner.interaktiv-tekstbaseret-spiller/spiller2"
       
-      "4" "local-clojure-agent://games.connect-four.interaktiv-tekstbaseret-spiller/spiller2"
-      "5" "local-clojure-agent://games.connect-four.interaktiv-grafikbaseret-spiller/spiller2"
-      "6" "local-clojure-agent://games.connect-four.ikke-interaktiv-spiller-baseret-paa-tilfaeldige-traek/spiller2"
+      "4" "lokal-clojureagent://games.connect-four.interaktiv-tekstbaseret-spiller/spiller2"
+      "5" "lokal-clojureagent://games.connect-four.interaktiv-grafikbaseret-spiller/spiller2"
+      "6" "lokal-clojureagent://games.connect-four.ikke-interaktiv-spiller-baseret-paa-tilfaeldige-traek/spiller2"
       "7" "http://localhost:3004"
-      "8" "local-clojure-agent://games.connect-four.ikke-interaktiv-spiller-baseret-paa-minimax/spiller2"
+      "8" "lokal-clojureagent://games.connect-four.ikke-interaktiv-spiller-baseret-paa-minimax/spiller2"
       "9" "http://localhost:3002"
       
-      "10" "local-clojure-agent://games.infection.interaktiv-tekstbaseret-spiller/spiller2"
-      "11" "local-clojure-agent://games.infection.interaktiv-grafikbaseret-spiller/spiller2"
-      "12" "local-clojure-agent://games.infection.ikke-interaktiv-spiller-baseret-paa-tilfaeldige-traek/spiller2"
+      "10" "lokal-clojureagent://games.infection.interaktiv-tekstbaseret-spiller/spiller2"
+      "11" "lokal-clojureagent://games.infection.interaktiv-grafikbaseret-spiller/spiller2"
+      "12" "lokal-clojureagent://games.infection.ikke-interaktiv-spiller-baseret-paa-tilfaeldige-traek/spiller2"
       "13" "http://localhost:3104"
-      "14" "local-clojure-agent://games.infection.ikke-interaktiv-spiller-baseret-paa-minimax/spiller2"
+      "14" "lokal-clojureagent://games.infection.ikke-interaktiv-spiller-baseret-paa-minimax/spiller2"
       "15" "http://localhost:3102"
       
-      "local-clojure-agent://games.draw-a-winner.ikke-interaktiv-spiller2/spiller2"
+      "lokal-clojureagent://games.draw-a-winner.ikke-interaktiv-spiller2/spiller2"
     )
   )
 )
@@ -109,29 +109,29 @@
   (println "\t*** Dommer ***")
   (println "\tValg\tBeskrivelse")
   (println "\t----\t-----------")
-  (println "\t1\t[Traek en vinder] Ikke-interaktiv dommer som \"local-clojure-agent\" (forudvalgt)")
+  (println "\t1\t[Traek en vinder] Ikke-interaktiv dommer som \"lokal-clojureagent\" (forudvalgt)")
   (println "\t2\t[Traek en vinder] Ikke-interaktiv dommer som \"http webservice paa localhost port 3200\"")
   (println "\t\t")
-  (println "\t3\t[Fire paa stribe] Ikke-interaktiv dommer som \"local-clojure-agent\"")
+  (println "\t3\t[Fire paa stribe] Ikke-interaktiv dommer som \"lokal-clojureagent\"")
   (println "\t4\t[Fire paa stribe] Ikke-interaktiv dommer som \"http webservice paa localhost port 3000\"")
   (println "\t\t")
-  (println "\t5\t[Infektion] Ikke-interaktiv dommer som \"local-clojure-agent\"")
+  (println "\t5\t[Infektion] Ikke-interaktiv dommer som \"lokal-clojureagent\"")
   (println "\t6\t[Infektion] Ikke-interaktiv dommer som \"http webservice paa localhost port 3100\"")
   (println "\tAndet\t*forudvalgt*")
   (print "\tVaelg dommer: ")
   (flush)
   (let [arbiter-choice (read-line)]
     (case arbiter-choice
-      "1" "local-clojure-agent://games.draw-a-winner.ikke-interaktiv-dommer/dommer"
+      "1" "lokal-clojureagent://games.draw-a-winner.ikke-interaktiv-dommer/dommer"
       "2" "http://localhost:3200"
       
-      "3" "local-clojure-agent://games.connect-four.ikke-interaktiv-dommer/dommer"
+      "3" "lokal-clojureagent://games.connect-four.ikke-interaktiv-dommer/dommer"
       "4" "http://localhost:3000"
       
-      "5" "local-clojure-agent://games.infection.ikke-interaktiv-dommer/dommer"
+      "5" "lokal-clojureagent://games.infection.ikke-interaktiv-dommer/dommer"
       "6" "http://localhost:3100"
       
-      "local-clojure-agent://games.draw-a-winner.ikke-interaktiv-dommer/dommer"
+      "lokal-clojureagent://games.draw-a-winner.ikke-interaktiv-dommer/dommer"
     )
   )
 )
